a98fde8cfd6d8e8f6b98020b7ffcaa282ada011c
KhageshorGiri <girikhageshor2432@gmail.com>
2025-08-16 14:38:12 +0545

    Setting up Domain Layer

---
+++ Backend/TicketManagement/src/Core/TicketManagement.Domain/Entities/Event.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TicketManagement.Domain.Common;
+
+namespace TicketManagement.Domain.Entities;
+
+public class Event : AuditableEntity
+{
+    [Key]
+    public Guid EventId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public string? Artist { get; set; }
+    public DateTime EventDate { get; set; }
+    public string? Description { get; set; }
+    public string? ImageUrl { get; set; }
+
+    [ForeignKey(nameof(Category))]
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; } = default!;
+}
